81011a5dd1c1392ae4189f84002fc43d0c46240d
yongseok <iyongseok@iyongseog-ui-MacBookPro.local>
2020-05-16 00:29:44 +0900

    add login

--- back/routes/user.js
+++ back/routes/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
+const passport = require('passport');
 const db = require('../models');
 
 const router = express.Router();
@@ -8,7 +9,6 @@ router.get('/', (req, res) => { // /api/user
 
 })
 router.post('/', async (req, res, next) => { //POST /api/user 회원가입
-  // POST /api/user 회원가입
   try {
     const exUser = await db.User.findOne({
       where: {
@@ -38,9 +38,26 @@ router.get('/:id', (req, res) => { // 남의 정보 가져오는 것 ex) /3
 router.post('/logout', (req, res) => { // /api/user/logout
 
 })
-router.post('/login', (req, res) => {
-  
-})
+router.post('/login', (req, res, next) => { // POST /api/user/login
+  passport.authenticate('local', (err, user, info) => {
+    if (err) {
+      console.error(err);
+      return next(err);
+    }
+    if (info) {
+      return res.status(401).send(info.reason);
+    }
+    return req.login(user, (loginErr) => {
+      if (loginErr) {
+        return next(loginErr);
+      }
+      const filteredUser = Object.assign({}, user.toJSON());
+      delete filteredUser.password;
+      return res.json(filteredUser);
+    });
+  })(req, res, next);
+});
+
 router.get('/:id/follow', (req, res) => {
 
 })
--- front/sagas/user.js
+++ front/sagas/user.js
@@ -1,28 +1,27 @@
 import {
-  all, call, put, fork, takeEvery, delay,
+  all, call, put, fork, takeEvery,
 } from 'redux-saga/effects';
 import axios from 'axios';
 import {
   LOG_IN_REQUEST, LOG_IN_SUCCESS, LOG_IN_FAILURE, SIGN_UP_REQUEST, SIGN_UP_SUCCESS, SIGN_UP_FAILURE,
 } from '../reducers/user';
 
+axios.defaults.baseURL = 'http://localhost:3065/api';
 
-// function loginAPI() {
-//   // 서버에 요청을 보내는 부분
-//   return axios.post('/login');
-// }
+function loginAPI(loginData) {
+  // 서버에 요청을 보내는 부분
+  return axios.post('/user/login', loginData);
+}
 
-function* login() {
+function* login(action) {
   try {
-    // yield call(loginAPI); // call 동기 fork 비동기
-    yield delay(2000);
+    yield call(loginAPI, action.data); // call 동기 fork 비동기
     yield put({
       // put은 dispatch 동일
       type: LOG_IN_SUCCESS,
     });
-  } catch (e) {
-    // loginAPI 실패
-    // console.error(e);
+  } catch (e) { // loginAPI 실패
+    console.error(e);
     yield put({
       type: LOG_IN_FAILURE,
     });
@@ -35,7 +34,7 @@ function* watchLogin() {
 
 function signUpAPI(signUpData) {
   // 서버에 요청을 보내는 부분
-  return axios.post('http://localhost:3065/api/user/', signUpData);
+  return axios.post('/user/', signUpData);
 }
 
 function* signUp(action) {
