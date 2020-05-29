6de4afaf1965a1e715d88568f7b9392722bd7a23
yongseok <iyongseok@iyongseog-ui-MacBookPro.local>
2020-05-29 22:59:12 +0900

    add image upload

--- front/components/PostCard.js
+++ front/components/PostCard.js
@@ -1,17 +1,11 @@
 import React, { useState, useCallback, useEffect } from 'react';
-import {
-  Button, Card, Avatar, Input, Form, List, Comment,
-} from 'antd';
+import { Button, Card, Avatar, Input, Form, List, Comment } from 'antd';
 import Link from 'next/link';
-import {
-  RetweetOutlined,
-  HeartOutlined,
-  MessageOutlined,
-  EllipsisOutlined,
-} from '@ant-design/icons';
+import { RetweetOutlined, HeartOutlined, MessageOutlined, EllipsisOutlined } from '@ant-design/icons';
 import PropTypes from 'prop-types';
 import { useSelector, useDispatch } from 'react-redux';
 import { ADD_COMMENT_REQUEST, LOAD_COMMENTS_REQUEST } from '../reducers/post';
+import PostImages from './PostImages';
 
 const PostCard = ({ post }) => {
   const [commentFormOpend, setCommentFormOpened] = useState(false);
@@ -56,7 +50,7 @@ const PostCard = ({ post }) => {
     <div>
       <Card
         key={+post.createdAt}
-        cover={post.img && <img alt="example" src={post.img} />}
+        cover={post.Images[0] && <PostImages images={post.Images} />}
         actions={[
           <RetweetOutlined key="retweet" />,
           <HeartOutlined key="heart" />,
