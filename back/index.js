cb1dd608e1bae7922c39fe0293fd2e9e8d24716b
yongseok <iyongseok@iyongseog-ui-MacBookPro.local>
2020-07-25 03:28:44 +0900

    Add: prod-ready packages

--- back/index.js
+++ back/index.js
@@ -5,6 +5,8 @@ const cookieParser = require('cookie-parser');
 const expressSession = require('express-session');
 const dotenv = require('dotenv');
 const passport = require('passport');
+const hpp = require('hpp');
+const helmet = require('helmet');
 
 const passportConfig = require('./passport');
 const db = require('./models');
@@ -20,12 +22,22 @@ db.sequelize.sync();
 // db.sequelize.sync({force:true});
 passportConfig();
 
-app.use(morgan('dev'));
+if (prod) {
+  app.use(hpp());
+  app.use(helmet());
+  app.use(morgan('combined'));
+  app.use(cors({
+    origin: 'http://nodebirdyong.cf',
+    credentials: true,
+  }))
+} else {
+  app.use(morgan('dev'));
+  app.use(cors({
+    origin: true,
+    credentials: true,
+  }));
+}
 app.use('/', express.static('uploads'));
-app.use(cors({
-  origin: true,
-  credentials: true,
-}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
@@ -36,6 +48,7 @@ app.use(expressSession({
   cookie: {
     httpOnly: true,
     secure: false, // https를 쓸 때 true
+    domain: prod && '.nodebirdyong.cf',
   },
   name: 'rnbck',
 }));
